8ba773813088e26e9e6430834a09c518898dba49
Yana Spirchina <spirchina04@gmail.com>
2023-03-12 15:23:32 +0300

    WIIIIIN

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -7,7 +7,5 @@ import ru.mail.polis.open.HelloWorld2;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        HelloWorld2.main(null);
     }
 }
